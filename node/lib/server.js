976c7f459db8942c767d77dfe029bc50bb62177b
Sheryl Boughton <sherpbo@gmail.com>
2015-05-02 09:27:23 -0700

    building 'facebook' with angular and node

---
+++ node/lib/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var Hapi = require('hapi');
+var Mongoose = require('mongoose');
+var Blipp = require('Blipp');
+var Plugins = require('./tools/plunge');
+var ServerConfig = require('./config/server.json');
+
+exports.init = function(port, cb){
+  var server = new Hapi.Server(ServerConfig);
+
+  server.connection({port: port});
+  Mongoose.connect(process.env.MONGO_URL);
+
+  Mongoose.connection.once('open', function(){
+    Plugins.push(Blipp);
+    server.register(Plugins, function(err){
+      if(err){return cb(err);}
+
+      server.start(function(err){
+        return cb(err, server);
+      });
+    });
+  });
+};
